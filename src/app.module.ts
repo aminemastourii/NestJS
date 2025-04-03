eceab22de9291bea3a4e020babb31f0e76e408f6
Amine Mastouri <amin.mastouri01@gmail.com>
2025-04-03 07:45:27 +0100

    fixing TypeORM err

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,27 +4,24 @@ import { AppService } from './app.service';
 import { BookingModule } from './booking/booking.module';
 import { PaymentModule } from './payment/payment.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
 import { Payment } from './payment/entities/payment.entity';
 
-@Module ({
-imports: [  TypeOrmModule.forRoot({
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '',
-  database: 'db_nestjs',
-  entities: [Payment],
-  synchronize: true,
-}),
-]
-})
+
+
 @Module({
-  imports: [BookingModule, PaymentModule,TypeOrmModule.forRoot() ],
+  imports: [BookingModule, PaymentModule,TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '',
+    database: 'db_nestjs',
+    entities: [Payment],
+    synchronize: true,
+  }) ],
   controllers: [AppController ],
   providers: [AppService],
 })
-export class AppModule {
-  constructor(private dataSource: DataSource) {}}
+export class AppModule {}
+
 
